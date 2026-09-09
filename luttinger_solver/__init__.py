74d8bf6dc9e04541f1aef88ea28bf13eb92117a9
Kelvin Dsouza <kelvindsouza91@gmail.com>
2026-09-09 13:29:03 -0400

    Delete luttinger_solver directory

--- luttinger_solver/__init__.py
+++
@@ -1,7 +0,0 @@
-from .lk_4band import LK4BandHamiltonian, LK_hamiltonian_4band
-from .lk_6band import LK_hamiltonian_6band
-from.lk_8band import LK_hamiltonian_8band
-
-
-__all__ = ["LK4BandHamiltonian", "LK_hamiltonian_4band",
-           "LK_hamiltonian_6band","LK_hamiltonian_8band"]
